577451a1ed02ce629f1e6fffe948572aa20b99d5
chenxinfeng <chenxinfeng@cibr.ac.cn>
2022-03-11 14:43:19 +0800

    v0.2

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 from setuptools import setup, find_packages
 setup(
     name='lilab', # 应用名
-    version='0.1', # 版本号
+    version='0.2', # 版本号
     packages=find_packages(include=['LILAB*']), # 包括在安装包内的 Python 包
     author='chenxf',
     author_email='cxf529125853@163.com',
+    url='https://pypi.org/project/lilab/'
 )
